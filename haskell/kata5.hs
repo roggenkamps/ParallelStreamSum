eadf50fb404e72d5abf23e496894869b34ab2b04
Steve Roggenkamp <roggenkamps@acm.org>
2014-01-09 00:14:22 -0500

    Refactored print to main.

--- haskell/kata5.hs
+++ haskell/kata5.hs
@@ -6,36 +6,27 @@
 import Data.Char
 
 main :: IO ()
-
 main = do
-  let inStr = "8745648184845171326578518184151512461752149647129746915414816354846454" in
-    iter inStr 100000
+  let inStr = "8745648184845171326578518184151512461752149647129746915414816354846454"
+      count = 1000000 in
+    print (iter inStr count)
 
-iter :: [Char] -> Int -> IO ()
+iter :: [Char] -> Int -> [[Char]]
 iter s 1 = analyzeString s
-iter s c =
-    do 
-      analyzeString s
-      iter s (c-1)
+iter s c = analyzeString s ++ iter s (c-1)
 
-analyzeString :: [Char] -> IO ()
-analyzeString [] = print ""
+analyzeString :: [Char] -> [[Char]]
+analyzeString [] =  [[]]
 analyzeString s =
-  do
     let x = hasSum0 "" s in
       if (fst x) /= "" then
-        do
-          print (fst x)
-          analyzeString (tail s)
+          [(fst x)] ++ analyzeString (tail s)
       else
         analyzeString (tail s)
-
-    
 
 hasSum0 :: [Char] -> [Char] -> ([Char],[Char])
 hasSum0 "" [] = ("", "")
 hasSum0 s [s0] = ("", "")
-hasSum0 s [s0,s1] = ("", "")
 hasSum0 s (s0:s1:r) = hasSum (digitToInt(s0)+digitToInt(s1)) [s0,s1] r
 
 hasSum :: Int -> [Char] -> [Char] -> ([Char],[Char])
