d9c660d611e635f5073e82dacf1aa4a17906ed84
Steve Roggenkamp <roggenkamps@acm.org>
2014-01-11 20:51:31 -0500

    Rewrote to process as a stream of Ints.  Still very slow.

---
+++ haskell/Kata/Par.hs
@@ -0,0 +1,50 @@
+module Kata.Par (
+  analyzeString,
+  analyzeInt,
+  completeSeq,
+  filter1,
+  filter2
+  )
+       where
+
+import Control.Exception
+import Control.Parallel
+import Control.Parallel.Strategies
+import Data.Char
+import Data.Maybe
+
+
+data SeqData = SD { val:: Int, str::[Int] }
+             | Out ([Int]) deriving (Show)
+
+analyzeString :: ([[Int]],[SeqData]) -> [Int] -> IO ()
+analyzeString (o,sd) [] = print o
+analyzeString (o,sd) s = 
+  let (o1,sd1) = analyzeInt (o,sd) (head s)
+    in
+      if not (null o1) then
+        do
+          print (head o1)
+          analyzeString ([],sd1) (tail s)
+      else
+        analyzeString (o1,sd1) (tail s)
+
+analyzeInt :: ([[Int]],[SeqData]) -> Int -> ([[Int]],[SeqData])
+analyzeInt (o,a) i  =
+  let x = map (completeSeq i) (a ++ [SD { val = 0, str = []}])
+      in ((o++(filter2 x)),(filter1 x))
+   
+completeSeq :: Int -> SeqData -> Maybe SeqData
+completeSeq i (SD {val=v, str=s} ) =
+   if v == i
+   then  Just (Out(s++[i]))
+   else if v > 9
+        then Nothing
+        else Just (SD { val = v+i, str=s++[i]})
+completeSeq i x = Nothing
+
+filter1 :: [Maybe SeqData] -> [SeqData]
+filter1 l = [SD{val=v,str=s} | Just (SD {val=v, str=s}) <- l ]
+
+filter2 :: [Maybe SeqData] -> [[Int]]
+filter2 l = [x | Just (Out x) <- l ]
--- haskell/kata5.hs
+++ haskell/kata5.hs
@@ -3,61 +3,20 @@
 -- Steve Roggenkamp
 --
 
-import Data.Char
+import Control.Exception
 import Control.Parallel
 import Control.Parallel.Strategies
-import Control.Exception
+import Data.Char
+import Data.Maybe
+import System.Environment
+import System.IO
+import Kata.Par
 
 main :: IO ()
 main = do
+  [cnt] <- getArgs 
   let inStr = "8745648184845171326578518184151512461752149647129746915414816354846454"
-      count = 1000000 in
-      iter (map digitToInt inStr) count
+      inInts = map digitToInt inStr
+      count = read cnt
+      in analyzeString ([],[])  (take count (cycle inInts))
 
-data SeqData = Active { val:: Int, str::[Int] }
-             | Complete [Int]
-             | NoSeq
-
-data PData = PData { out :: [[Int]], seqs :: [SeqData] }
-
-iter :: [Int] -> Int -> IO ()
-iter s 1 = print (analyzeString s)
-iter s c = (do print x; y)
-           where x = analyzeString s
-                 y = iter s (c-1)
-
-completeSeq :: Int -> SeqData -> SeqData
-completeSeq i (Active {val=v, str=s} ) =
-   if v == i
-   then Complete (s++[i])
-   else if v > 9
-        then NoSeq
-        else Active { val = v+i, str=s++[i]}
-completeSeq i (Complete s) = Complete s
-completeSeq i NoSeq      = NoSeq
-
-analyzeString :: [Int] -> [[Int]]
-analyzeString [] =  [[]]
-analyzeString s = out(foldl analyzeInt PData { out=[], seqs=[] } s)
-
-analyzeInt :: PData -> Int -> PData
-analyzeInt PData{ out=o, seqs=a } i  =
-  let x = map (completeSeq i) (a ++ [Active { val = 0, str = []}])
-  in
-   PData { out  = o++(map extractSeqs (filter filterP0 x)),
-           seqs = filter filterP1 x }
-
-extractSeqs :: SeqData -> [Int]
-extractSeqs (Active {val=_, str=_ }) = []
-extractSeqs (Complete x)      = x
-extractSeqs NoSeq             = []
-
-filterP0 :: SeqData -> Bool
-filterP0 (Active {val=_, str=_ }) = False
-filterP0 (Complete x)      = True
-filterP0 NoSeq             = False
-
-filterP1 :: SeqData -> Bool
-filterP1 (Active {val= _, str=_ }) = True
-filterP1 (Complete x)      = False
-filterP1 NoSeq             = False
