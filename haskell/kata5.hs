84b98c31533955efcd5c968e3bf0dbc133bfe0b2
Steve Roggenkamp <roggenkamps@acm.org>
2014-01-11 22:20:43 -0500

    Moved Char to Int transform to main.  Saved about 1.5s.  Run time now 5.55 s.

--- haskell/kata5.hs
+++ haskell/kata5.hs
@@ -9,32 +9,32 @@ main :: IO ()
 main = do
   let inStr = "8745648184845171326578518184151512461752149647129746915414816354846454"
       count = 1000000 in
-    print (iter inStr count)
+    print (iter (map digitToInt inStr) count)
 
-iter :: [Char] -> Int -> [[Char]]
+iter :: [Int] -> Int -> [[Int]]
 iter s 1 = analyzeString s
 iter s c = analyzeString s ++ iter s (c-1)
 
-analyzeString :: [Char] -> [[Char]]
+analyzeString :: [Int] -> [[Int]]
 analyzeString [] =  [[]]
 analyzeString s =
-    let x = hasSum0 "" s in
-      if (fst x) /= "" then
+    let x = hasSum0 [] s in
+      if (fst x) /= [] then
           [(fst x)] ++ analyzeString (tail s)
       else
         analyzeString (tail s)
 
-hasSum0 :: [Char] -> [Char] -> ([Char],[Char])
-hasSum0 "" [] = ("", "")
-hasSum0 s [s0] = ("", "")
-hasSum0 s (s0:s1:r) = hasSum (digitToInt(s0)+digitToInt(s1)) [s0,s1] r
+hasSum0 :: [Int] -> [Int] -> ([Int],[Int])
+hasSum0 [] [] = ([], [])
+hasSum0 s [s0] = ([], [])
+hasSum0 s (s0:s1:r) = hasSum (s0+s1) [s0,s1] r
 
-hasSum :: Int -> [Char] -> [Char] -> ([Char],[Char])
-hasSum v s [] = ("","")
+hasSum :: Int -> [Int] -> [Int] -> ([Int],[Int])
+hasSum v s [] = ([],[])
 hasSum v s l = if v > 9
-               then ("",l)
+               then ([],l)
                else
-	         let x = digitToInt (head l) in
+	         let x = head l in
                    if  v == x
                    then (s++[(head l)], tail(l))
 	  	   else hasSum (v+x) (s++[(head l)]) (tail l)
