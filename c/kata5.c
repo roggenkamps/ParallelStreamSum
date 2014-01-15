88f3e2d5043164e485a1ccc9089472f25cc0760b
Steve Roggenkamp <roggenkamps@acm.org>
2014-01-14 22:05:27 -0500

    Added C version of kata5.

---
+++ c/kata5.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+#define MAXVALS 10
+
+struct state_data {
+  uint64_t icount;
+  uint8_t  values[MAXVALS];
+  uint8_t  digits[MAXVALS];
+  uint8_t  nactive;
+};
+
+int analyzeDigits( uint8_t digit, struct state_data *sdata )
+{
+  int index = sdata->icount % MAXVALS;
+  int startIndex = index;
+  int i;
+  uint8_t maxloop = (sdata->nactive < MAXVALS) ? sdata->nactive : MAXVALS;
+  sdata->digits[index] = digit;
+  sdata->values[index] = digit;
+  for ( i = 0; i < maxloop; i++ ) {
+    /*    int lindex = index+i+1; */
+    int lindex = index-maxloop+i;
+    if ( lindex < 0 ) {
+      lindex = lindex + MAXVALS;
+    }
+    if ( sdata->values[lindex] == digit ) {
+      int v = digit;
+      int j = lindex;
+      while ( v > 0 ) {
+	printf( "%d", sdata->digits[j]);
+	j++;
+	if ( j >= MAXVALS ) { j += MAXVALS; }
+	v -= sdata->digits[j];
+      }
+      printf( "%d\n", digit );
+    }
+    if ( sdata->values[lindex] != 255 ) {
+      sdata->values[lindex] += digit;
+      if ( sdata->values[lindex] > 9 ) {
+	sdata->values[lindex] = 255;
+	sdata->nactive--;
+      }
+    }
+  }
+  
+  sdata->icount++;
+  if ( sdata->nactive < MAXVALS ) { sdata->nactive++; }
+  return 1;
+}
+
+int main( int argc, char **argv )
+{
+  static char instr[] = "8745648184845171326578518184151512461752149647129746915414816354846454";
+    /* static char instr[] = "24617"; */
+  uint8_t  digits[sizeof(instr)];
+  uint64_t count = 0;
+  uint64_t digitIndex = 0;
+  uint64_t inLength = sizeof( instr )-1;
+  struct state_data  *sdata;
+  long   i;
+  
+  if ( argc != 2 ) {
+    fprintf( stderr, "usage:  kata5 count\n" );
+  }
+
+  count = strtol( *(argv+1), NULL, 0 );
+
+  /* allocate memory */
+  sdata = (struct state_data *) mmap(NULL,
+				     sizeof(struct state_data),
+				     PROT_READ|PROT_WRITE,
+				     MAP_PRIVATE | MAP_ANONYMOUS | MAP_LOCKED | MAP_POPULATE,
+				     -1, 0
+				     );
+
+  if ( sdata == MAP_FAILED ) {
+    perror( "map failed" );
+    exit( EXIT_FAILURE );
+  }
+
+  for ( i = 0; i < inLength; i++ ) {
+    digits[i] = instr[i] - '0';
+  }
+
+  for ( i = 0; i < count; i++ ) {
+    digitIndex = i % inLength;
+    if ( analyzeDigits( digits[digitIndex], sdata ) == -1 ) {
+      exit( EXIT_FAILURE );
+    }
+  }
+
+  exit( EXIT_SUCCESS );
+}
+
--- haskell/kata5.hs
+++ haskell/kata5.hs
@@ -3,40 +3,39 @@
 -- Steve Roggenkamp
 --
 
+import Control.Exception
+import Control.Parallel.Strategies
 import Data.Char
 import Data.List
 import System.Environment
 
 main :: IO ()
 main = do
-  [cnt] <- getArgs 
+  [cnt,bsz] <- getArgs 
   let inStr = "8745648184845171326578518184151512461752149647129746915414816354846454"
       count = read cnt
+      batchsize = read bsz
       inInts = map digitToInt inStr
     in
-      print (analyzeString (take count (cycle inInts)))
+      print (analyzeString batchsize (take count (cycle inInts)))
 
-analyzeString :: [Int] -> [[Int]]
-analyzeString [] =  [[]]
-analyzeString s = filter (\y -> not (null y)) (map (\x -> fst (hasSum0 [] x)) (tails s))
---    let x = hasSum0 [] s in
---      if (fst x) /= [] then
---          [(fst x)] ++ analyzeString (tail s)
---      else
---        analyzeString (tail s)
+analyzeString :: Int -> [Int] -> [[Int]]
+analyzeString b [] = []
+analyzeString b s = (filter (\y -> not (null y))
+                  (map (\x -> fst (hasSum0 [] x)) (take b (tails s)) `using` parList rseq )) ++ (analyzeString b (drop b s))
 
 hasSum0 :: [Int] -> [Int] -> ([Int],[Int])
-hasSum0 [] [] = ([], [])
-hasSum0 s [s0] = ([], [])
+hasSum0 [] []       = ([], [])
+hasSum0 s [s0]      = ([], [])
 hasSum0 s (s0:s1:r) = hasSum (s0+s1) [s0,s1] r
 
 hasSum :: Int -> [Int] -> [Int] -> ([Int],[Int])
 hasSum v s [] = ([],[])
-hasSum v s l = if v > 9
-               then ([],l)
-               else
-	         let x = head l in
-                   if  v == x
-                   then (s++[(head l)], tail(l))
-	  	   else hasSum (v+x) (s++[(head l)]) (tail l)
+hasSum v s l  = if v > 9
+                then ([],l)
+                 else
+	           let x = head l in
+                       if  v == x
+                       then (s++[(head l)], tail(l))
+	  	       else hasSum (v+x) (s++[(head l)]) (tail l)
                  
