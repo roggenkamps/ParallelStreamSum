95827995cf88ba03a8daca1694d5e5b0f9f3f3fd
Steve Roggenkamp <roggenkamps@acm.org>
2014-01-15 23:33:29 -0500

    Rewrote to implement register-level parallelism. Reduced run time by about 1/3.

--- c/kata5.c
+++ c/kata5.c
@@ -8,63 +8,70 @@
 
 struct state_data {
   uint64_t icount;
-  uint8_t  values[MAXVALS];
-  uint8_t  digits[MAXVALS];
-  uint8_t  nactive;
+  uint64_t  values;
+  uint64_t  digits;
+  uint64_t  multiplier;
+  int       nactive;
+  char      outbuf[4096];
+  int       outsz;
 };
+
+int val( uint64_t x, int i ) {
+  return (( x & ((uint64_t)0x1f << (i*5))) >> (i*5));
+}
 
 int analyzeDigits( uint8_t digit, struct state_data *sdata )
 {
-  int index = sdata->icount % MAXVALS;
-  int startIndex = index;
-  int i;
-  char out[10];
-  uint8_t maxloop = (sdata->nactive < MAXVALS) ? sdata->nactive : MAXVALS;
-  sdata->digits[index] = digit;
-  sdata->values[index] = digit;
-  for ( i = 0; i < maxloop; i++ ) {
-    /*    int lindex = index+i+1; */
-    int lindex = index-maxloop+i;
-    if ( lindex < 0 ) {
-      lindex = lindex + MAXVALS;
-    }
-    if ( sdata->values[lindex] == digit ) {
-      int v = digit;
-      int j = lindex;
-      int k = 0;
-      while ( v > 0 ) {
-	out[k]=sdata->digits[j]+'0';
-	j++;
-	k++;
-	if ( j >= MAXVALS ) { j += MAXVALS; }
-	v -= sdata->digits[j];
+  int v         = 0;
+  int      out  = 0;
+  uint64_t mask = 0;
+  int      i;
+  sdata->multiplier = (sdata->multiplier << 5) + 1;
+  uint64_t delta = sdata->multiplier * digit;
+  sdata->values  <<= 5;
+  sdata->digits  = (sdata->digits << 5) + digit;
+  
+  for (i=sdata->nactive; i > 1; i-- ) {
+    if (((v = (sdata->values & (mask=(((uint64_t)0x1f) << (i*5)))) >> (i*5)) == digit )) {
+      int      ov = v;
+      uint64_t m  = mask;
+      int      n  = i;
+      while ( ov >= 0 ) {
+	int d = (sdata->digits & m) >> (n*5);
+	sdata->outbuf[sdata->outsz++] = d + '0';
+	ov -= d;
+	m >>= 5;
+	n--;
       }
-      out[k] =0;
-      printf( "%s\n", out );
-    }
-    if ( sdata->values[lindex] != 255 ) {
-      sdata->values[lindex] += digit;
-      if ( sdata->values[lindex] > 9 ) {
-	sdata->values[lindex] = 255;
-	sdata->nactive--;
+      sdata->outbuf[sdata->outsz++] = '\n';
+      if ( sdata->outsz > sizeof( sdata->outbuf ) - 32 ) {
+	fwrite( sdata->outbuf, sdata->outsz, 1, stdout );
+	sdata->outsz = 0;
       }
     }
   }
-  
+  sdata->values += delta;
+  while ((v = (sdata->values & (mask=(((uint64_t)0x1f) << (sdata->nactive*5)))) >> (sdata->nactive*5)) > 9 ) {
+    sdata->values = sdata->values ^ (v << (sdata->nactive*5));
+    sdata->digits = sdata->digits ^ (sdata->digits & mask);
+    sdata->multiplier >>= 5;
+    sdata->nactive--;
+  }
+  sdata->nactive++;
   sdata->icount++;
-  if ( sdata->nactive < MAXVALS ) { sdata->nactive++; }
   return 1;
 }
 
 int main( int argc, char **argv )
 {
+  /*                                    |--|        |-|   |-|-|                                */
   static char instr[] = "8745648184845171326578518184151512461752149647129746915414816354846454";
-    /* static char instr[] = "24617"; */
+  /* static char instr[] = "111111111911248124617"; */
   uint8_t  digits[sizeof(instr)];
   uint64_t count = 0;
   uint64_t digitIndex = 0;
   uint64_t inLength = sizeof( instr )-1;
-  struct state_data  *sdata;
+  struct state_data  sdata;
   long   i;
   
   if ( argc != 2 ) {
@@ -74,27 +81,19 @@ int main( int argc, char **argv )
   count = strtol( *(argv+1), NULL, 0 );
 
   /* allocate memory */
-  sdata = (struct state_data *) mmap(NULL,
-				     sizeof(struct state_data),
-				     PROT_READ|PROT_WRITE,
-				     MAP_PRIVATE | MAP_ANONYMOUS | MAP_LOCKED | MAP_POPULATE,
-				     -1, 0
-				     );
-
-  if ( sdata == MAP_FAILED ) {
-    perror( "map failed" );
-    exit( EXIT_FAILURE );
-  }
-
   for ( i = 0; i < inLength; i++ ) {
     digits[i] = instr[i] - '0';
   }
 
   for ( i = 0; i < count; i++ ) {
     digitIndex = i % inLength;
-    if ( analyzeDigits( digits[digitIndex], sdata ) == -1 ) {
+    if ( analyzeDigits( digits[digitIndex], &sdata ) == -1 ) {
       exit( EXIT_FAILURE );
     }
+  }
+
+  if ( sdata.outsz > 0 ) {
+    fwrite( sdata.outbuf, sdata.outsz, 1, stdout );
   }
 
   exit( EXIT_SUCCESS );
