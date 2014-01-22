9475c547e31507ed15a76abb366612a878ebfe75
Steve Roggenkamp <roggenkamps@acm.org>
2014-01-22 07:36:37 -0500

    Revert "Rewrote extensively and added Kata/Kata.hs"
    This reverts commit 0820e94be851ed4a6127d13838289255a64fa98e.
    
    Now have it on branch b005.

--- haskell/kata5.hs
+++ haskell/kata5.hs
@@ -6,16 +6,36 @@
 import Control.Exception
 import Control.Parallel.Strategies
 import Data.Char
+import Data.List
 import System.Environment
-import Kata.Kata
-
 
 main :: IO ()
 main = do
-  [cnt] <- getArgs 
+  [cnt,bsz] <- getArgs 
   let inStr = "8745648184845171326578518184151512461752149647129746915414816354846454"
-    -- inStr = "111111111911248124617"
       count = read cnt
+      batchsize = read bsz
       inInts = map digitToInt inStr
     in
-      print (analyzeString (take count (cycle inInts)))
+      print (analyzeString batchsize (take count (cycle inInts)))
+
+analyzeString :: Int -> [Int] -> [[Int]]
+analyzeString b [] = []
+analyzeString b s = (filter (\y -> not (null y))
+                  (map (\x -> fst (hasSum0 [] x)) (take b (tails s)) `using` parList rseq )) ++ (analyzeString b (drop b s))
+
+hasSum0 :: [Int] -> [Int] -> ([Int],[Int])
+hasSum0 [] []       = ([], [])
+hasSum0 s [s0]      = ([], [])
+hasSum0 s (s0:s1:r) = hasSum (s0+s1) [s0,s1] r
+
+hasSum :: Int -> [Int] -> [Int] -> ([Int],[Int])
+hasSum v s [] = ([],[])
+hasSum v s l  = if v > 9
+                then ([],l)
+                 else
+	           let x = head l in
+                       if  v == x
+                       then (s++[(head l)], tail(l))
+	  	       else hasSum (v+x) (s++[(head l)]) (tail l)
+                 
